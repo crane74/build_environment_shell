e48e8dd11aaa4646f3057a3fa2adbf2f61222f31
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-10-04 15:00:37 +0900

    chore: Singurality環境構築用のスクリプトを追加

---
+++ singurality.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+# Singularity 環境構築用のシェルスクリプト(Singularity v3.10.0)
+
+# 依存関係のインストール
+sudo apt update && sudo apt install -y \
+    build-essential \
+    libssl-dev \
+    uuid-dev \
+    libgpgme11-dev \
+    squashfs-tools \
+    libseccomp-dev \
+    wget \
+    pkg-config \
+    git
+
+# Goのインストール
+export VERSION=1.17.2 OS=linux ARCH=amd64 && \
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
+
+# Goの 環境設定(バージョンは適宜変更)
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
+    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
+    source ~/.bashrc
+
+# リリースからSingularityCEをインストール(バージョンは適宜変更)
+# export VERSION=3.10.0 && # adjust this as necessary \
+#     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz && \
+#     tar -xzf singularity-ce-${VERSION}.tar.gz && \
+#     cd singularity-ce-${VERSION}
+
+# ソースからSinguralityをインストール
+git clone https://github.com/sylabs/singularity.git && \
+    cd singularity && \
+    git checkout v3.9.5
+
+# Singuralityをコンパイル
+./mconfig && \
+    make -C ./builddir && \
+    sudo make -C ./builddir install
+
+# bash補完
+. /usr/local/etc/bash_completion.d/singularity
