4297b613257053f73469fe794efb0e16385801ec
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-10-07 20:02:31 +0900

    fix: fix script for singularity

--- singurality.sh
+++ singurality.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Singularity 環境構築用のシェルスクリプト(Singularity v3.10.0)
+# Singularity 環境構築用のシェルスクリプト(Singularity v3.9.5)
 
 # 依存関係のインストール
 sudo apt update && sudo apt install -y \
@@ -14,16 +14,18 @@ sudo apt update && sudo apt install -y \
     pkg-config \
     git
 
-# Goのインストール
+# Goのインストール(バージョンは適宜変更)
 export VERSION=1.17.2 OS=linux ARCH=amd64 && \
     wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
     sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
     rm go$VERSION.$OS-$ARCH.tar.gz
 
-# Goの 環境設定(バージョンは適宜変更)
+# Goの 環境設定
 echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
     echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-    source ~/.bashrc
+    export GOPATH=${HOME}/go && \
+    export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin
+
 
 # リリースからSingularityCEをインストール(バージョンは適宜変更)
 # export VERSION=3.10.0 && # adjust this as necessary \
@@ -43,3 +45,6 @@ git clone https://github.com/sylabs/singularity.git && \
 
 # bash補完
 . /usr/local/etc/bash_completion.d/singularity
+
+# singularityリポジトリを削除
+rm -rf ../singularity/
